454b3c7382ea24f296860e8c5e95ff0a8eddd02e
alamin-alsaafin <aassc-1996@hotmail.com>
2018-07-09 16:15:12 +0300

    add sign-up form

---
+++ competition.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8"/>
+<!-- /Added by HTTrack -->
+
+<?php
+@include ('style.php')
+?>
+
+
+<!--
+    Additional Classes:
+        .nk-page-boxed
+-->
+
+<body>
+
+
+<?php
+@include ('header.php')
+?>
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content competition-join-us">
+      <h3 style="text-align: center !important;margin-top: 7% !important;color:#dd163b;">اشترك معنا الان !</h3>
+      <h5 style="margin-right: 5%;">الاسم كاملا</h5>
+      <input type="text" name="" style="height: 40px !important;background: #fff !important;border: none !important;border: 1px solid #dd163b !important;color: #000 !important;padding-right: 20px !important;margin-bottom: 3%;width: 90%; margin-right: 6%;border-radius: 10px;">
+      <h5 style="margin-right: 5%;">رقم الهاتف المحمول</h5>
+      <input type="number" name="" style="height: 40px !important;background: #fff !important;border: none !important;border: 1px solid #dd163b !important;color: #000 !important;padding-right: 20px !important;margin-bottom: 3%;width: 90%; margin-right: 6%;border-radius: 10px;">
+      <h5 style="margin-right: 5%;">البريد الالكتروني</h5>
+      <input type="text"  name="" style="height: 40px !important;background: #fff !important;border: none !important;border: 1px solid #dd163b !important;color: #000 !important;padding-right: 20px !important;margin-bottom: 3%;width: 90%; margin-right: 6%;border-radius: 10px;">
+      <h5 style="margin-right: 5%;">الدولة</h5>
+      <input type="text"  name="" style="height: 40px !important;background: #fff !important;border: none !important;border: 1px solid #dd163b !important;color: #000 !important;padding-right: 20px !important;margin-bottom: 3%;width: 90%; margin-right: 6%;border-radius: 10px;">
+      <button type="submit" style="width: 50%;border: none;background: #dd163b;color: #fff;font-size: 21px;border-radius: 10px;margin-bottom: 5%;margin-top: 4%;margin-right: 25%;">اشترك الان</button>
+    </div>
+  </div>
+</div>
+
+<div class="container comp-box">
+    <div class="row">
+        <div class="col-xs-12">
+            <h3 class="title">
+             COMPETITONS
+            </h3>
+        </div>
+    </div>
+
+    <div class="row comp">
+        <div class="col-sm-4  col-xs-12">
+            <a href="#"><img src="assets/images/post-7-mid-square.jpg" class="img-responsive partner-logo"></a>
+
+        </div>
+        <div class="col-sm-8 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            </p>
+            <button type="text" data-toggle="modal" data-target="#myModal">Join Us</button>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row comp">
+        <div class="col-sm-4 col-xs-12">
+            <a href="#"><img src="assets/images/post-7-mid-square.jpg" class="img-responsive partner-logo"></a>
+        </div>
+        <div class="col-sm-8 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+            </p>
+            <button type="text" data-toggle="modal" data-target="#myModal">Join Us</button>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row comp">
+        <div class="col-sm-4 col-xs-12">
+           <a href="#"> <img src="assets/images/post-7-mid-square.jpg" class="img-responsive partner-logo"></a>
+        </div>
+        <div class="col-sm-8 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+            </p>
+            <button type="text" data-toggle="modal" data-target="#myModal">Join Us</button>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row comp">
+        <div class="col-sm-4 col-xs-12">
+            <a href="#"><img src="assets/images/post-7-mid-square.jpg" class="img-responsive partner-logo"></a>
+        </div>
+        <div class="col-sm-8 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            </p>
+            <button type="text" data-toggle="modal" data-target="#myModal">Join Us</button>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row comp">
+        <div class="col-sm-4 col-xs-12">
+           <a href="#"> <img src="assets/images/post-7-mid-square.jpg" class="img-responsive partner-logo"></a>
+        </div>
+        <div class="col-sm-8 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            </p>
+            <button type="text" data-toggle="modal" data-target="#myModal">Join Us</button>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row comp">
+        <div class="col-sm-4 col-xs-12">
+           <a href="#"> <img src="assets/images/post-7-mid-square.jpg" class="img-responsive partner-logo"></a>
+        </div>
+        <div class="col-sm-8 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+            </p>
+            <button type="text" data-toggle="modal" data-target="#myModal">Join Us</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <nav aria-label="Page navigation example" style="margin-left: 31%;
+    margin-top: 3%;" class="pag-pag">
+              <ul class="pagination">
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Previous" style="color: #fff !important;
+    background-color: #dd163b !important;">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#" style="color: #fff !important;
+    background-color: #dd163b !important;">1</a></li>
+                <li class="page-item"><a class="page-link" href="#" style="color: #fff !important;
+    background-color: #dd163b !important;">2</a></li>
+                <li class="page-item"><a class="page-link" href="#" style="color: #fff !important;
+    background-color: #dd163b !important;">3</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Next" style="color: #fff !important;
+    background-color: #dd163b !important;">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+
+
+    <?php
+    @include ('footer.php')
+    ?>
+
+
+</div>
+
+
+<!-- START: Page Background -->
+
+<img class="nk-page-background-top" src="assets/images/bg-top.png" alt="">
+<img class="nk-page-background-bottom" src="assets/images/bg-bottom.png" alt="">
+
+<!-- END: Page Background -->
+
+
+<?php
+@include ('javascript.php')
+?>
+
+</body>
+
+</html>
---
+++ partners.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8"/>
+<!-- /Added by HTTrack -->
+
+<?php
+@include ('style.php')
+?>
+
+
+<!--
+    Additional Classes:
+        .nk-page-boxed
+-->
+
+<body>
+
+
+<?php
+@include ('header.php')
+?>
+
+<div class="container partners-box">
+    <div class="row">
+        <div class="col-xs-12">
+            <h3 class="title">
+                Our Partners
+            </h3>
+        </div>
+    </div>
+    <div class="row partner">
+        <div class="col-md-3 col-xs-12">
+            <img src="assets/images/GoHunt.jpg" class="img-responsive partner-logo">
+        </div>
+        <div class="col-md-9 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            </p>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row partner">
+        <div class="col-md-3 col-xs-12">
+            <img src="assets/images/GoHunt.jpg" class="img-responsive partner-logo">
+        </div>
+        <div class="col-md-9 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+            </p>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row partner">
+        <div class="col-md-3 col-xs-12">
+            <img src="assets/images/GoHunt.jpg" class="img-responsive partner-logo">
+        </div>
+        <div class="col-md-9 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+            </p>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row partner">
+        <div class="col-md-3 col-xs-12">
+            <img src="assets/images/GoHunt.jpg" class="img-responsive partner-logo">
+        </div>
+        <div class="col-md-9 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            </p>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row partner">
+        <div class="col-md-3 col-xs-12">
+            <img src="assets/images/GoHunt.jpg" class="img-responsive partner-logo">
+        </div>
+        <div class="col-md-9 col-xs-12">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            </p>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row partner">
+        <div class="col-md-3 col-xs-12">
+            <img src="assets/images/GoHunt.jpg" class="img-responsive partner-logo">
+        </div>
+        <div class="col-md-9 col-xs-12 hidden-xs ">
+            <p class="description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+            </p>
+        </div>
+    </div>
+    <hr class="line1">
+    <div class="row">
+        <div class="col-xs-12">
+            <nav aria-label="Page navigation example" style="margin-left: 31%;
+    margin-top: 3%;" class="pag-pag">
+              <ul class="pagination">
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Previous" style="color: #fff !important;
+    background-color: #dd163b !important;">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#" style="color: #fff !important;
+    background-color: #dd163b !important;">1</a></li>
+                <li class="page-item"><a class="page-link" href="#" style="color: #fff !important;
+    background-color: #dd163b !important;">2</a></li>
+                <li class="page-item"><a class="page-link" href="#" style="color: #fff !important;
+    background-color: #dd163b !important;">3</a></li>
+                <li class="page-item">
+                  <a class="page-link" href="#" aria-label="Next" style="color: #fff !important;
+    background-color: #dd163b !important;">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+
+
+    <?php
+    @include ('footer.php')
+    ?>
+
+
+</div>
+
+
+<!-- START: Page Background -->
+
+<img class="nk-page-background-top" src="assets/images/bg-top.png" alt="">
+<img class="nk-page-background-bottom" src="assets/images/bg-bottom.png" alt="">
+
+<!-- END: Page Background -->
+
+
+<?php
+@include ('javascript.php')
+?>
+
+</body>
+
+</html>
